dac0fab550f58469696653b9479e51a5c3a9517e
Himanshu Sharma <himanshu90sharma@gmail.com>
2020-02-06 14:15:19 -0600

    Added a test for the data size check

--- MNISTdata2NPZWrite.py
+++ MNISTdata2NPZWrite.py
@@ -2,23 +2,29 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import struct
-from sklearn.model_selection import train_test_split
 import argparse
 
 parser = argparse.ArgumentParser(description='Write the 8M MNIST files in NPZ format image files')
 parser.add_argument('-p', '--path_data', type=str, default='./')
 parser.add_argument('-f', '--file_name', type=str, default='mnist60k-')
 parser.add_argument('-o', '--out_path', type=str, default='./DATA')
-parser.add_argument('-c', '--check', type=bool, default=True)
-
+parser.add_argument('-c', '--check', type=bool, default=False)
+    
 args = parser.parse_args()
 
+
+def Check_SampleSize(Sample, Label):
+    if (Sample.shape)[0] == len(Label):
+        val = True
+    else:
+        val = False
+    return val 
 
 
 LOC = {'loc' : os.path.join(args.path_data,(args.file_name+'labels')),
        'loc_image' : os.path.join(args.path_data,(args.file_name+'pattern'))
       }
-
+                   
 
 # Label Loading
 with open(LOC['loc'],'rb') as file, open(LOC['loc_image'], 'rb') as file_image:
@@ -27,11 +33,15 @@ with open(LOC['loc'],'rb') as file, open(LOC['loc_image'], 'rb') as file_image:
     N, = struct.unpack('>i', file.read(4))
     if N != struct.unpack('>i', file_image.read(4))[0]:
         raise RuntimeError('wrong pair of MNIST images and labels')
-
+    
     file_image.read(8)
     images = np.empty((N, 784), dtype=np.uint8)
     labels = np.empty(N, dtype=np.uint8)
 
+    test_index = N - int(0.2 *N) # 20 percent of the
+    
+    print (test_index)
+    
     for i in range(N):
         labels[i] = ord(file.read(1))
         for j in range(784):
@@ -40,16 +50,25 @@ with open(LOC['loc'],'rb') as file, open(LOC['loc_image'], 'rb') as file_image:
     # Reshaping array with 1 channel form
     images = images.reshape((N,28,28,1))
 
-train_x, test_x, train_y, test_y = train_test_split(images,labels,test_size=0.2)
+train_x, test_x, train_y, test_y = images[0:test_index,:,:,:], images[test_index:,:,:,:], labels[0:test_index], labels[test_index:]
 
 
 filename= os.path.join(args.out_path, 'Training_images.npz')
-np.savez_compressed(filename, Train_x=train_x, Test_x=test_x, Train_y=train_y,Test_y=test_y)
+np.savez_compressed(filename, 
+                    Train_x=train_x, 
+                    Test_x=test_x, 
+                    Train_y=train_y,
+                    Test_y=test_y)
 
 if args.check:
     with np.load(filename) as file:
-        out = ( np.array_equal(train_x,file['Train_x']) ) *  ( np.array_equal(test_x,file['Test_x']) )*         ( np.array_equal(train_y,file['Train_y']) ) * ( np.array_equal(test_y,file['Test_y']) )
-    print ('Check Passed ...!! .npz file successfully written')
+        out = ( np.array_equal(train_x,file['Train_x']) ) * ( np.array_equal(test_x,file['Test_x']) )*( np.array_equal(train_y,file['Train_y']) ) * ( np.array_equal(test_y,file['Test_y']) )
+        out_Len_Train = Check_SampleSize(file['Train_x'],file['Train_y'])
+        out_Len_Test = Check_SampleSize(file['Test_x'],file['Test_y'])
+        
+        out_ = (out_Len_Train * out_Len_Test)
 
-    if out == 0:
+    print ('Check Passed ...!! .npz file successfully written')
+    
+    if out == 0 or out_ == 0:
         raise RuntimeError('Script Failed...!!')
